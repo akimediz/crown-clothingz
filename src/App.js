077d738d14150ccbcf4798a8b81bfa6233927263
Morakinyo Abdulakeem <morakinyoabdulakeem@gmail.com>
2022-06-07 06:16:45 -0700

    update our menu-item to navigate our withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatPage = ()=>{
+  return (
+   <div>
+      Hats Page ...
+
+   </div>
+  )
+}
 
 function App() {
   return (
     <div >
-    <Homepage />
+      <Switch>
+        <Route  exact path="/" component={Homepage}/>
+        <Route  exact path="/shop/hats" component={HatPage}/>
+
+      </Switch>
     </div>
   );
 }
